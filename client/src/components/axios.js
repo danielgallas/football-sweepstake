860afc01587c0919df7534f579393639a54f9de4
Daniel Gallas <danielgallas2@gmail.com>
2023-04-30 11:28:59 +0100

    changes server address to vercel in client/axios

--- client/src/components/axios.js
+++ client/src/components/axios.js
@@ -2,8 +2,12 @@ import axios from "axios";
 
 // const instance = axios.create({ baseURL: "http://localhost:5000" });
 
+// const instance = axios.create({
+//   baseURL: "https://football-sweepstake.onrender.com",
+// });
+
 const instance = axios.create({
-  baseURL: "https://football-sweepstake.onrender.com",
+  baseURL: "https://football-sweepstake.vercel.app/api/v1/scores",
 });
 
 export default instance;
